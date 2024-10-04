15c320b8d452e72ef12b191cb08f2083a1c51f25
Luke Kerbs <LDK.kerbs@gmail.com>
2024-10-04 12:30:23 -0700

    Bugfix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="scriptmonkey",
-    version="0.1.1",  # Incremented version
+    version="0.1.2",
     description="A Python package that automatically fixes errors in your code using OpenAI's GPT API",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
@@ -11,8 +11,8 @@ setup(
     author_email="LDK.kerbs@gmail.com",
     license="MIT",
     packages=find_packages(),
-    install_requires=["openai", "pydantic", "tqdm"],
-    python_requires=">=3.6",  # Minimum Python version
+    install_requires=["openai", "pydantic", "tqdm", "python-dotenv"],
+    python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
